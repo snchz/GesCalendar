ef3a8a0074b51e9bce24f374589229d621f07251
snchz <snchz@example.com>
2016-11-07 22:45:04 +0100

    Modelo logico 1.0

--- src/Calendario.java
+++ src/Calendario.java
@@ -5,14 +5,45 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
-public class Calendario{
+public class Calendario implements Iterable<Dia>{
 	private List<Dia> _calendario;
 
-	public Calendario(){
-		_calendario=new ArrayList<Dia>();
+	public Calendario() {
+		_calendario = new ArrayList<Dia>();
+	}
+	
+	public Calendario(File fichero) {
+		this();
+		importarCalendario(fichero);
+	}
+	
+	/**
+	 * Agrega el dia.
+	 * 
+	 * @param d
+	 * @return true si todo ok
+	 */
+	public boolean agregarDia(Dia d) {
+		return _calendario.add(d);
+	}
+
+	/**
+	 * 
+	 * @param d
+	 * @param tipo
+	 * @param horas
+	 * @return false, si no existe el dia. true, si existe.
+	 * @throws Exception 
+	 */
+	public boolean agregarHorasADia(Dia d, String tipo, double horas) throws Exception{
+		Dia temp=obtenerDia(d);
+		if (temp==null)
+			return false;
+		else
+			return temp.agregarHoras(tipo, horas);
 	}
 	
 	/**
@@ -20,127 +51,147 @@ public class Calendario{
 	 * @param fichero
 	 * @return
 	 */
-	public boolean importarCalendario(File fichero){
-		boolean res=false;
-		_calendario=new ArrayList<Dia>();
+	private boolean importarCalendario(File fichero) {
+		boolean res = false;
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(fichero));
-			String linea=br.readLine();
-			while (linea!=null){
-				Dia d=new Dia(linea);
-				_calendario.add(d);
-				linea=br.readLine();
+			String linea = br.readLine();
+			while (linea != null) {
+				Dia d = new Dia(linea);
+				this.agregarDia(d);
+				linea = br.readLine();
 			}
 			br.close();
-			res=true;
+			res = true;
 		} catch (Exception e) {
-			res=false;
+			res = false;
 			e.printStackTrace();
 		}
 		return res;
 	}
-	
-	public boolean generarCalendario(Dia desde, Dia hasta){
-		boolean res=true;
-		Dia ini,fin;
-		if (desde.hashCode()<=hasta.hashCode()){
-			ini=new Dia(desde);
-			fin=new Dia(hasta);
-		}else{
-			ini=new Dia(hasta);
-			fin=new Dia(desde);
-		}
-		while (ini.hashCode()<=fin.hashCode()){
-			_calendario.add(ini);
-			ini=ini.obtenerDiaSiguiente();
+
+	public boolean generarCalendario(Dia desde, Dia hasta) {
+		boolean res = true;
+		try {
+			Dia ini, fin;
+			if (desde.hashCode() <= hasta.hashCode()) {
+				ini = new Dia(desde);
+				fin = new Dia(hasta);
+			} else {
+				ini = new Dia(hasta);
+				fin = new Dia(desde);
+			}
+			while (ini.hashCode() <= fin.hashCode()) {
+				this.agregarDia(ini);
+				ini = ini.obtenerDiaSiguiente();
+			}
+			res = true;
+		} catch (Exception e) {
+			res = false;
 		}
 		return res;
 	}
-	
-	public boolean exportarCalendario(File fichero){
-		boolean res=false;
+
+	public boolean exportarCalendario(File fichero) {
+		boolean res = false;
 		try {
 			BufferedWriter bw = new BufferedWriter(new FileWriter(fichero));
-			for(Dia d:_calendario)
-				bw.append(d.toString());
+			for (Dia d : _calendario)
+				bw.append(d.toString()+"\n");
 			bw.close();
-			res=true;
+			res = true;
 		} catch (Exception e) {
-			res=false;
+			res = false;
 			e.printStackTrace();
 		}
 		return res;
 	}
-	
-	
+
 	/**
-	 * Agrega el dia, si existe lo sustituye.
-	 * @param d
-	 * @return true si todo ok
-	 */
-	public boolean agregarDia(Dia d){
-		_calendario.remove(d);
-		return _calendario.add(d);
-	}
-	
-	/**
-	 * Obtiene el día.
+	 * Obtiene el dia. 
+	 * 
 	 * @param dia
 	 * @param mes
-	 * @param año
+	 * @param  anio
 	 * @return nulo si no existe
 	 */
-	public Dia obtenerDia(int dia, int mes, int año){
+	public Dia obtenerDia(Dia d) {
 		Dia temp;
 		try {
-			temp = new Dia(dia, mes, año, new HashMap<String, Double>());
+			temp = new Dia(d.obtenerDia(), d.obtenerMes(), d.obtenerAnio());
 		} catch (Exception e) {
 			return null;
 		}
-		for (Dia d:_calendario)
-			if (temp.equals(d))
-				return d;
+		for (Dia di : _calendario)
+			if (temp.equals(di))
+				return di;
 		return null;
 	}
-	
+
 	@Override
 	public String toString() {
 		this.ordenar();
-		StringBuffer res=new StringBuffer();
-		for (Dia d:_calendario)
-			res.append(d+"\n");
+		StringBuffer res = new StringBuffer();
+		for (Dia d : _calendario)
+			res.append(d + "\n");
 		return res.toString();
 	}
 
-	@SuppressWarnings("unchecked")
 	private void ordenar() {
 		Collections.sort(_calendario);
 	}
-
-	public static void main(String[] args) {
-		Calendario c=new Calendario();
-		System.out.println("Importando...");
-		c.importarCalendario(new File("exportado.dat"));
-		System.out.println("Importado");
-		Dia d=null;
-		try {
-			d = new Dia(8, 8, 2016, new HashMap<>());
-		} catch (Exception e) {
-			e.printStackTrace();
+	
+	public Double obtenerSumaHoras(String tipo, Dia ini, Dia fin) {
+		double res=0;
+		for (Dia d:_calendario){
+			if (d.hashCode()>=ini.hashCode()&&d.hashCode()<=fin.hashCode())
+				res=res+d.obtenerSumaHoras(tipo);
 		}
-		c.agregarDia(d);
-		try {
-			System.out.println("Generando...");
-			c.generarCalendario(new Dia("20160101"), new Dia("20161231"));
-			System.out.println("Generado");
-		} catch (Exception e) {
-			e.printStackTrace();
+		return res;
+	}
+	
+	public List<Dia> obtenerDiasSinHoras(){
+		List<Dia> res=new ArrayList<>();
+		for (Dia d:_calendario){
+			if (d.obtenerSumaTotalHoras()==0)
+				res.add(d);
 		}
-		System.out.println("Exportando...");
-		c.exportarCalendario(new File("exportado.dat"));
-		System.out.println("Exportado");
-		System.out.println(c);
+		Collections.sort(res);
+		return res;
+	}
+	
+	public int obtenerContadorDiasConTipo(String tipo, Dia ini, Dia fin) {
+		int res=0;
+		for (Dia d:_calendario){
+			if (d.hashCode()>=ini.hashCode()&&d.hashCode()<=fin.hashCode()&&d.tieneHorasDelTipo(tipo))
+				res++;
+		}
+		return res;
+	}
+	
+	public boolean rellenarFinDeSemana(String tipo, double horas) throws Exception{
+		for(Dia d:_calendario){
+			if (d.obtenerDiaDeLaSemana()==6 || d.obtenerDiaDeLaSemana()==7){
+				d.agregarHoras(tipo, horas);
+			}
+		}
+		return true;
 	}
 
+	public static void main(String[] args) {
+		try {
+			Calendario c = new Calendario();
+			c.importarCalendario(new File("exportado.dat"));
+			System.out.println(c);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public Iterator<Dia> iterator() {
+		return _calendario.iterator();
+	}
 
 }
--- src/Dia.java
+++ src/Dia.java
@@ -4,143 +4,183 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-public class Dia implements Comparable<Dia>{
+public class Dia implements Comparable<Dia> {
 	private Map<String, Double> _horas;
-	private int _dia, _mes, _año;
+	private int _dia, _mes, _anio;
 
-	public Dia(int dia, int mes, int año, Map<String, Double> horas) throws Exception {
+	public Dia(int dia, int mes, int anio) throws Exception{
 		_dia = dia;
 		_mes = mes;
-		_año = año;
-		_horas=new HashMap<String, Double>();
-		_horas.putAll(horas);
+		_anio = anio;
+		this.asignarHoras(new HashMap<String, Double>());
 		if (!esValido())
-			throw new Exception("Fecha inválida: " + this.hashCode());
+			throw new Exception("Fecha invalida: " + this.hashCode());
 	}
 	
-	public Dia(Dia dia){
-		_dia=dia.getDia();
-		_mes=dia.getMes();
-		_año=dia.getAño();
-		_horas=new HashMap<String, Double>(dia.getHoras());
+	public Dia(int dia, int mes, int anio, Map<String, Double> horas) throws Exception {
+		this(dia,mes,anio);
+		this.asignarHoras(horas);
+	}
+
+	public Dia(Dia dia) throws Exception {
+		this(dia.obtenerDia(),dia.obtenerMes(),dia.obtenerAnio(),new HashMap<String, Double>(dia.obtenerHoras()));
 	}
 	
 	/**
-	 * Con el formato del toString
-	 * 20160101#EXTRA=1.0;INVIERNO=8.0
-	 * 20160229#
-	 * @param dia
-	 * @throws Exception 
+	 * 
+	 * @return 1 Lunes, 2 Martes, 3 Miercoles, 4 Jueves, 5 Viernes, 6 Sabado, 7 Domingo
 	 */
-	public Dia(String diahoras) throws Exception{
-		try{
-			StringTokenizer st=new StringTokenizer(diahoras,"#");
-			String fecha=st.nextToken();//YYYYMMDD
-			setFecha(fecha);
-			_horas=new HashMap<String, Double>();
+	public int obtenerDiaDeLaSemana(){
+		LocalDate ld = LocalDate.of(this._anio, this._mes, this._dia);
+		return ld.getDayOfWeek().getValue();
+	}
+
+	/**
+	 * Con el formato del toString 20160101#EXTRA=1.0;INVIERNO=8.0 20160229#
+	 * 
+	 * @param dia
+	 * @throws Exception
+	 */
+	public Dia(String registro) throws Exception {
+		try {
+			StringTokenizer st = new StringTokenizer(registro, "#");
+			String fecha = st.nextToken();// YYYYMMDD
+			asignarFecha(fecha);
+			_horas = new HashMap<String, Double>();
 			String lista;
 			if (st.hasMoreElements())
-				lista=st.nextToken();//EXTRA=1.0;INVIERNO=8.0 ó  (vacio)
+				lista = st.nextToken();// EXTRA=1.0;INVIERNO=8.0 ï¿½ (vacio)
 			else
-				lista="";
-			st=new StringTokenizer(lista,";");
-			while (st.hasMoreTokens()){
-				String tipo_horas=st.nextToken();
-				StringTokenizer stth=new StringTokenizer(tipo_horas,"=");
-				String tipo=stth.nextToken();
-				String horas=stth.nextToken();
+				lista = "";
+			st = new StringTokenizer(lista, ";");
+			while (st.hasMoreTokens()) {
+				String tipo_horas = st.nextToken();
+				StringTokenizer stth = new StringTokenizer(tipo_horas, "=");
+				String tipo = stth.nextToken();
+				String horas = stth.nextToken();
 				_horas.put(tipo, Double.valueOf(horas));
 			}
-		}catch (Exception e) {
-			throw new Exception("String de Dia con formato incorrecto "+e.getMessage());
+		} catch (Exception e) {
+			throw new Exception("String de Dia con formato incorrecto " + e.getMessage());
 		}
 	}
 
 	/**
 	 * 
-	 * @param fecha con formato YYYYMMDD
+	 * @param fecha
+	 *            con formato YYYYMMDD
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	private boolean setFecha(String fecha) throws Exception {
-		try{
-			int ifecha=Integer.parseInt(fecha);
-			_año=ifecha/10000;
-			_mes=(ifecha-_año*10000)/100;
-			_dia=ifecha-_año*10000-_mes*100;
-		}catch (Exception e) {
-			throw new Exception("Fecha inválida: " + this.hashCode());
+	private boolean asignarFecha(String fecha) throws Exception {
+		try {
+			int ifecha = Integer.parseInt(fecha);
+			_anio = ifecha / 10000;
+			_mes = (ifecha - _anio * 10000) / 100;
+			_dia = ifecha - _anio * 10000 - _mes * 100;
+		} catch (Exception e) {
+			throw new Exception("Fecha invalida: " + this.hashCode());
 		}
 		return true;
 	}
-	
-	public Dia obtenerDiaSiguiente(){
-		 try {
-			 LocalDate ld=LocalDate.of(this._año, this._mes, this._dia);
-			 LocalDate ldsig=ld.plusDays(1);
-			 Dia res=new Dia(ldsig.getDayOfMonth(), ldsig.getMonthValue(), ldsig.getYear(), new HashMap<>());
+
+	public Dia obtenerDiaSiguiente() {
+		try {
+			LocalDate ld = LocalDate.of(this._anio, this._mes, this._dia);
+			LocalDate ldsig = ld.plusDays(1);
+			Dia res = new Dia(ldsig.getDayOfMonth(), ldsig.getMonthValue(), ldsig.getYear(), new HashMap<>());
 			return new Dia(res);
 		} catch (Exception e) {
 			return null;
 		}
 	}
 
-	public int getDia() {
+	public int obtenerDia() {
 		return _dia;
 	}
 
-	public int getMes() {
+	public int obtenerMes() {
 		return _mes;
 	}
 
-	public int getAño() {
-		return _año;
+	public int obtenerAnio() {
+		return _anio;
 	}
 
-	public Map<String, Double> getHoras() {
+	public Map<String, Double> obtenerHoras() {
 		return _horas;
 	}
 
-	public Double getTotalHoras() {
-		double res=0;
-		for (Double d:_horas.values())
-			res=res+d;
+	public Double obtenerSumaTotalHoras() {
+		double res = 0;
+		for (Double d : _horas.values())
+			res = res + d;
 		return res;
 	}
 
-	public Double getHoras(String tipo) {
-		return _horas.get(tipo);
+	public Double obtenerSumaHoras(String tipo) {
+		Double temp=_horas.get(tipo);
+		double res=0;
+		if (temp==null)
+			res=0;
+		else
+			res=temp.doubleValue();
+		return res;
+	}
+	
+	public boolean tieneHorasDelTipo(String tipo){
+		return _horas.containsKey(tipo);
 	}
 
-	public boolean setHoras(Map<String, Double> horas) {
-		_horas=new HashMap<String, Double>();
+	/**
+	 * Setea las horas con el contenido de horas
+	 * @param horas
+	 * @return
+	 * @throws Exception 
+	 */
+	public boolean asignarHoras(Map<String, Double> horas) throws Exception {
+		_horas = new HashMap<String, Double>();
 		_horas.putAll(horas);
+		if (this.obtenerSumaTotalHoras()>24)
+			throw new Exception("Mas de 24 horas al dia no es posible.");
+		return true;
+	}
+	
+	public boolean agregarHoras(String tipo, double horas) throws Exception {
+		_horas.put(tipo, horas);
+		if (this.obtenerSumaTotalHoras()>24)
+			throw new Exception("Mas de 24 horas al dia no es posible: "+this.toString());
 		return true;
 	}
 
 	private boolean esValido() {
 		boolean valido = true;
-	    try {
-	        LocalDate.of(_año, _mes, _dia);
-	    } catch (DateTimeException e) {
-	    	valido = false;
-	    }
-	    return valido;
+		try {
+			LocalDate.of(_anio, _mes, _dia);
+		} catch (DateTimeException e) {
+			valido = false;
+		}
+		return valido;
 	}
 
 	@Override
+	/**
+	 * Ejemplo:
+	 * 20160101#EXTRA=1.0;INVIERNO=8.0
+	 * 20160229#
+	 */
 	public String toString() {
-		StringBuffer sb=new StringBuffer();
+		StringBuffer sb = new StringBuffer();
 		sb.append(this.hashCode());
 		sb.append("#");
-		for (String tipo:_horas.keySet()){
+		for (String tipo : _horas.keySet()) {
 			sb.append(tipo);
 			sb.append("=");
 			sb.append(_horas.get(tipo));
 			sb.append(";");
 		}
-		if (sb.substring(sb.length()-1, sb.length()).equals(";"))
-			sb.delete(sb.length()-1, sb.length());
+		if (sb.substring(sb.length() - 1, sb.length()).equals(";"))
+			sb.delete(sb.length() - 1, sb.length());
 		return sb.toString();
 	}
 
@@ -150,8 +190,8 @@ public class Dia implements Comparable<Dia>{
 	 */
 	public int hashCode() {
 		// 20161130
-		//2016 1 1 -> 20160000+100+1=20160101
-		int res = _año * 10000 + _mes * 100 + _dia;
+		// 2016 1 1 -> 20160000+100+1=20160101
+		int res = _anio * 10000 + _mes * 100 + _dia;
 		return res;
 	}
 
@@ -167,42 +207,15 @@ public class Dia implements Comparable<Dia>{
 		return other.hashCode() == this.hashCode();
 	}
 
-	public static void main(String[] args) {
-		try {
-			Dia d1=new Dia("20160101#EXTRA=1.0;INVIERNO=8.0");
-			Dia d2=new Dia("20160229#");
-			System.out.println(d1);
-			System.out.println(d2);
-			
-			/*
-			HashMap<String, Double> horas=new HashMap<String, Double>();
-			horas.put("INVIERNO", 8.0);
-			horas.put("EXTRA", 1.0);
-			Dia d1 = new Dia(1, 1, 2016, horas);
-			System.out.println(d1);
-
-			HashMap<String, Double> horas12=new HashMap<String, Double>();
-			Dia d12 = new Dia(29, 2, 2016, horas12);
-			System.out.println(d12);
-			
-			HashMap<String, Double> horas2=new HashMap<String, Double>();
-			horas2.put("INVIERNO", 8.0);
-			Dia d2 = new Dia(30, 2, 2016, horas2);
-			System.out.println(d2);*/
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	@Override
 	/**
-	 * Número positivo: la cadena 1 es mayor que la cadena 2.
-	 * 0: las cadenas son iguales.
-	 * Número negativo: la cadena 1 es menor que la cadena 2.
+	 * Numero positivo: la cadena this es mayor que la cadena o. 
+	 * 0: las cadenas son guales. 
+	 * Numero negativo: la cadena this es menor que la cadena o.
 	 * @param arg0
 	 * @return
 	 */
 	public int compareTo(Dia o) {
-		return this.hashCode()-o.hashCode();
+		return this.hashCode() - o.hashCode();
 	}
 }
